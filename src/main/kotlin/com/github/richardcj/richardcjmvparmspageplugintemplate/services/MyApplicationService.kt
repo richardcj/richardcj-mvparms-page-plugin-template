859f9ddff623435d8aee67199fe6100eb44fcc93
GitHub Action <action@github.com>
2021-08-15 02:00:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/richardcj/richardcjmvparmspageplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.richardcj.richardcjmvparmspageplugintemplate.services
+
+import com.github.richardcj.richardcjmvparmspageplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/richardcj/richardcjmvparmspageplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/richardcj/richardcjmvparmspageplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.richardcj.richardcjmvparmspageplugintemplate.services
 
+import com.github.richardcj.richardcjmvparmspageplugintemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
